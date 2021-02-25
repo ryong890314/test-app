fb5a16c798266f8f0d3355e9ec5dcd0f169e8444
ryong890314 <ryong890314@gmail.com>
2021-02-25 17:43:23 +0900

    2102251743 react-app init

---
+++ react-app/src/Menu.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+function Menu() {
+  return (
+    <div className="Menu">
+      <ul>
+        <li>대시보드</li>
+        <li>통계</li>
+        <li>고객</li>
+        <li>마케팅</li>
+        <li>설정</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
